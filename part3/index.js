384fba44b4b85e36d897dce0fb8cfb779b8b0a6d
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-08-05 15:03:09 +0800

    commit changes

--- part3/index.js
+++ part3/index.js
@@ -1,30 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-require('dotenv').config()
-const mongoose = require('mongoose')
 
-const password = process.argv[2]
-console.log(process.env.MONGODB_URI)
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -54,11 +33,10 @@ let notes = [
 
 const cors = require('cors')
 
-
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
-app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -70,50 +48,59 @@ app.get('/api/notes', (request, response) => {
   })
 })
   
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }})
+      .catch(error => next(error))
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
-
-const generateID = () => {
-  const maxID = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-
-  return String(maxID + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error:'content missing'
     })
   }
   
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+
+  
+})
+
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
+
   const note = {
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateID(),
+    important: body.important
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  .then(updatedNote => {
+    response.json(updatedNote)
+  })
+  .catch(error => next(error))
 })
 
 // Wrong url message
@@ -123,6 +110,19 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+// Wrong/nonexistant id error
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
